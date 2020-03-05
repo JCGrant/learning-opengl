2a97be1046e5bd5e46110477acaab2171b126924
JCGrant <jamescolin.grant@gmail.com>
2020-03-05 12:42:22 +0000

    Render 10 rotating cubes

--- src/main.cpp
+++ src/main.cpp
@@ -95,6 +95,15 @@ int main() {
       -0.5f, 0.5f,  0.5f,  0.0f, 0.0f, //
       -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f  //
   };
+
+  // world space positions of our cubes
+  glm::vec3 cubePositions[] = {
+      glm::vec3(0.0f, 0.0f, 0.0f),    glm::vec3(2.0f, 5.0f, -15.0f),
+      glm::vec3(-1.5f, -2.2f, -2.5f), glm::vec3(-3.8f, -2.0f, -12.3f),
+      glm::vec3(2.4f, -0.4f, -3.5f),  glm::vec3(-1.7f, 3.0f, -7.5f),
+      glm::vec3(1.3f, -2.0f, -2.5f),  glm::vec3(1.5f, 2.0f, -2.5f),
+      glm::vec3(1.5f, 0.2f, -1.5f),   glm::vec3(-1.3f, 1.0f, -1.5f)};
+
   unsigned int VBO, VAO;
   glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
@@ -169,10 +178,6 @@ int main() {
     glActiveTexture(GL_TEXTURE1);
     glBindTexture(GL_TEXTURE_2D, texture2);
 
-    glm::mat4 model = glm::mat4(1.0f);
-    model =
-        glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.1f));
-
     glm::mat4 view = glm::mat4(1.0f);
     view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 
@@ -181,12 +186,21 @@ int main() {
         100.0f);
 
     shader.use();
-    shader.setMat4("model", model);
     shader.setMat4("view", view);
     shader.setMat4("projection", projection);
 
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 36);
+    for (unsigned int i = 0; i < 10; i++) {
+      glm::mat4 model = glm::mat4(1.0f);
+      model = glm::translate(model, cubePositions[i]);
+      float angle = 20.0f * i;
+      model =
+          glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+      model =
+          glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.1f));
+      shader.setMat4("model", model);
+      glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
 
     glfwSwapBuffers(window);
     glfwPollEvents();
