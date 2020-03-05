d5d70685dd13d6d278d4c80330b1b26f7a19983d
JCGrant <jamescolin.grant@gmail.com>
2020-03-05 14:07:50 +0000

    Rotate camera around boxes

--- src/main.cpp
+++ src/main.cpp
@@ -167,6 +167,11 @@ int main() {
   shader.setInt("texture1", 0);
   shader.setInt("texture2", 1);
 
+  glm::mat4 projection = glm::perspective(
+      glm::radians(45.0f), (float)DEFAULT_WIDTH / (float)DEFAULT_HEIGHT, 0.1f,
+      100.0f);
+  shader.setMat4("projection", projection);
+
   while (!glfwWindowShouldClose(window)) {
     processInput(window);
 
@@ -178,16 +183,15 @@ int main() {
     glActiveTexture(GL_TEXTURE1);
     glBindTexture(GL_TEXTURE_2D, texture2);
 
-    glm::mat4 view = glm::mat4(1.0f);
-    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-
-    glm::mat4 projection = glm::perspective(
-        glm::radians(45.0f), (float)DEFAULT_WIDTH / (float)DEFAULT_HEIGHT, 0.1f,
-        100.0f);
-
     shader.use();
+
+    glm::mat4 view = glm::mat4(1.0f);
+    float radius = 10.0f;
+    float camX = sin(glfwGetTime()) * radius;
+    float camZ = cos(glfwGetTime()) * radius;
+    view = glm::lookAt(glm::vec3(camX, 0.0f, camZ), glm::vec3(0.0f, 0.0f, 0.0f),
+                       glm::vec3(0.0f, 1.0f, 0.0f));
     shader.setMat4("view", view);
-    shader.setMat4("projection", projection);
 
     glBindVertexArray(VAO);
     for (unsigned int i = 0; i < 10; i++) {
@@ -196,8 +200,6 @@ int main() {
       float angle = 20.0f * i;
       model =
           glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-      model =
-          glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.1f));
       shader.setMat4("model", model);
       glDrawArrays(GL_TRIANGLES, 0, 36);
     }
